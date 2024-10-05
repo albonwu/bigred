d63770979841507afd6cb4f1d88997420e668e99
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-10-05 00:53:26 -0400

    initial cli commit

---
+++ tex-equations/dist/background.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('This is background service worker - edit me!');
+
+/******/ })()
+;
+//# sourceMappingURL=background.js.map
