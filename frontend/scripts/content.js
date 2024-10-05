f8010709081580287fb233a36f986d8d047b52da
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-10-05 00:12:59 -0400

    make extension add click event listener to show mathml code

--- frontend/scripts/content.js
+++ frontend/scripts/content.js
@@ -1,1 +1,45 @@
 console.log("hello world from the eq -> speech extension!");
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function msFrom(date) {
+  return new Date().getTime() - date.getTime();
+}
+const timeoutMs = 20 * 1000;
+const sleepMs = 1000;
+
+async function main() {
+  let equationDivs = document.querySelectorAll("div[data-type=equation]");
+  const startDate = new Date();
+
+  while (msFrom(startDate) < timeoutMs) {
+    console.log("refresh");
+    equationDivs = document.querySelectorAll("div[data-type=equation]");
+    if (equationDivs.length === 0) {
+      await sleep(sleepMs);
+    } else {
+      break;
+    }
+  }
+
+  if (equationDivs.length === 0) {
+    console.log("no equations found!");
+    return;
+  }
+  console.log("equationDivs", equationDivs);
+
+  for (const equationDiv of equationDivs) {
+    console.log("equationDiv", equationDiv);
+    const mathMl = equationDiv.querySelector(
+      'script[type="math/mml"]'
+    )?.innerHTML;
+    if (mathMl) {
+      equationDiv.addEventListener("click", () =>
+        console.log("mathMl", mathMl)
+      );
+    }
+  }
+}
+
+main();
