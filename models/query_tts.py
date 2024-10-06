5b74f9790f59af96ce39ab05dcc02b255e4b819a
Albon Wu <albonwu@umich.edu>
2024-10-06 14:16:20 -0400

    include system prompt

--- models/query_tts.py
+++ models/query_tts.py
@@ -6,14 +6,23 @@ from vertexai.generative_models import GenerativeModel, SafetySetting, Part
 def multiturn_generate_content():
     vertexai.init(project="862482900034", location="us-central1")
     model = GenerativeModel(
-        "projects/862482900034/locations/us-central1/endpoints/6392262636238536704",
+        "projects/862482900034/locations/us-central1/endpoints/6969849288448802816",
+        system_instruction=[textsi_1]
     )
     chat = model.start_chat()
+    print(chat.send_message(
+        ["""C = \\sqrt[3]{\\frac{\\Delta_{1} \\pm \\sqrt{\\Delta_{1}^{2} - 4\\Delta_{0}^{3}}}{2}}"""],
+        generation_config=generation_config,
+        safety_settings=safety_settings
+    ))
 
+textsi_1 = """You are the helpful engine behind a text-to-speech application for LaTeX. Your job is to take a piece of LaTeX as input and convert it to natural language. You should output nothing except raw text consisting of the mathematical expression translated into words.
+
+Be as unambiguous as possible - students and mathematicians with vision impairment rely on your translation to learn, teach, and conduct research. Simultaneously, do not be overly verbose; use as few words as possible to convey unambiguity. Respect order of operations, and replicate the original expression completely faithfully."""
 
 generation_config = {
     "max_output_tokens": 8192,
-    "temperature": 1,
+    "temperature": 0.2,
     "top_p": 0.95,
 }
 
