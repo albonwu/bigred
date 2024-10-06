fb7a9c0a7951c1de08106f3ef226f6dbfc53bf41
albonwu <albonwu@umich.edu>
2024-10-06 00:29:52 -0400

    data shenanigans

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+from cartesia import Cartesia
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
+
+client = Cartesia(api_key=os.environ.get("CARTESIA_API_KEY"))
 
 
 @app.route("/", methods=["GET", "POST"])
