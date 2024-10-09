1892c1d05e7a2dad25d037c6a4e3ae618e150f21
Wayne He <28408948+Ecpii@users.noreply.github.com>
2024-10-09 19:27:47 -0400

    update streamlit ui

--- backend/stream.py
+++ backend/stream.py
@@ -1,5 +1,10 @@
+import os
 import requests
 import streamlit as st
+from time import time
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define your CSS
 style = """
@@ -13,21 +18,59 @@ style = """
 # Inject CSS with markdown
 st.markdown(style, unsafe_allow_html=True)
 
+st.image("header.png")
+
+"Convert LaTeX expressions to natural audio!"
+
 code = st.text_area(
-    "LaTeX Input",
+    "LaTeX Input:",
     placeholder="|\\psi\\rangle = \\alpha|0\\rangle + \\beta|1\\rangle",
 )
 
 
-def handle_submit():
+def find_from_pinata_cache(tex):
     response = requests.get(
-        f"http://127.0.0.1:5000/?tex={code}",
+        f"https://api.pinata.cloud/v3/files?metadata[tex]={tex}",
+        headers={
+            "authorization": f'Bearer {os.environ.get("PINATA_API_JWT")}'
+        },
     )
-    print(f"{response.text = }")
-    st.session_state["audio_url"] = response.text
+    response_json = response.json()
+    response_files = response_json["data"]["files"]
+    if response_files:
+        cid = response_files[0]["cid"]
+        # create signed file
+        PINATA_GATEWAY = os.getenv("PINATA_GATEWAY")
+        if not PINATA_GATEWAY:
+            print("Error: PINATA_GATEWAY not defined environment variable")
+            return
+        response = requests.post(
+            "https://api.pinata.cloud/v3/files/sign",
+            headers={
+                "authorization": f'Bearer {os.environ.get("PINATA_API_JWT")}',
+            },
+            json={
+                "url": f"https://{PINATA_GATEWAY}/files/{cid}",
+                "expires": 24 * 3600,
+                "date": int(time()),
+                "method": "GET",
+            },
+        )
+        return response.json()["data"]
 
 
-st.button("Submit", on_click=handle_submit)
+def handle_submit():
+    signed_url = find_from_pinata_cache(code)
+    if not signed_url:
+        response = requests.get(
+            f"http://127.0.0.1:5000/?tex={code}",
+        )
+        print(f"{response.text = }")
+        signed_url = response.text
+    st.session_state["audio_url"] = signed_url
+
+
+st.button("Read!", on_click=handle_submit)
 
 if "audio_url" in st.session_state:
-    st.audio(st.session_state["audio_url"])
+    st.audio(st.session_state["audio_url"], autoplay=True)
